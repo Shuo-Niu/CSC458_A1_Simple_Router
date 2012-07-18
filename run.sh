9915f31b0cd2629d07184eca240892d8c85dc967
Te-Yuan Huang <huangty@stanford.edu>
2012-07-18 23:38:46 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+sudo ip rule add from 10.0.1.0/24 table cs144
+sudo ip route add default dev eth1 table cs144
+sudo ip route flush cache
+sudo ip route list table cs144
+sudo ip rule list
+
 screen -S mininet -D -R sudo python mininet.py
 screen -S pox -D -R ./pox/pox.py cs144.ofhandler cs144.srhandler
+

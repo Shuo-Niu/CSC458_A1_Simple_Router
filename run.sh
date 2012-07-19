88c89d0514adcc5ec4065886cd3472e8d5f43a7e
Te-Yuan Huang <huangty@stanford.edu>
2012-07-19 00:04:58 +0000

    add run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ sudo ip route flush cache
 sudo ip route list table cs144
 sudo ip rule list
 
-screen -S mininet -D -R sudo python mininet.py
+screen -S mininet -D -R sudo python lab3.py
 screen -S pox -D -R ./pox/pox.py cs144.ofhandler cs144.srhandler
 

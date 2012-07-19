e88d0a93c3b752d5001869fc7256857b15ada251
Te-Yuan Huang <huangty@stanford.edu>
2012-07-19 02:54:18 +0000

    push source routing rule in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-RULE_INSTALLED=`sudo ip rule list | grep cs144`
-if [ ! -n $RULE_INSTALLED ]; then 
+RULE_INSTALLED=`sudo ip rule list | grep cs144 | awk '{print $3}'`
+if [ $RULE_INSTALLED != '10.0.1.0/24' ]; then 
   echo Installing source routing rule
   sudo ip rule add from 10.0.1.0/24 table cs144
   sudo ip route add default dev eth1 table cs144
   sudo ip route flush cache
   sudo ip route list table cs144
   sudo ip rule list
+else
+  echo routing table is ready
 fi
 
 screen -S mininet -D -R sudo python lab3.py

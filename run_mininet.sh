e2934306ad96ee026d7fbf1062c9a183f0330351
Te-Yuan Huang <huangty@stanford.edu>
2012-12-01 00:13:23 -0800

    remove bridge

--- run_mininet.sh
+++ run_mininet.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-BR_INSTALLED=`sudo ovs-vsctl show | grep br0 | grep Bridge | awk '{print $2}'`
-if [ ! -n "$BR_INSTALLED" ]; then
-  echo setup bridge br0
-  sudo ovs-vsctl add-br br0
-  sudo ovs-vsctl add-port br0 eth1
-  sudo ovs-vsctl add-port br0 root-eth0
-  sudo ovs-vsctl show
-else
-  echo bridge is ready
-fi
 sudo python lab3.py

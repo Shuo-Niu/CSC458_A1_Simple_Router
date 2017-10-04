9b6070fb1c8584dfae6a9807169fa4172ed4cbc9
DESKTOP-PV4VR8Q\40737 <shuo.niu@mail.utoronto.ca>
2017-10-04 14:33:14 -0400

    test

--- router/sr_router.c
+++ router/sr_router.c
@@ -76,7 +76,7 @@ void sr_handlepacket(struct sr_instance* sr,
   assert(packet);
   assert(interface);
 
-  printf("*** -> SHUO Received packet of length %d \n",len);
+  printf("*** -> Received packet of length %d \n",len);
 
   /* fill in code here */
 
